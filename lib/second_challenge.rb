4582ff64037f4a2c7d865c37ec38dd67c8415ac4
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-29 23:22:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,13 @@ def second_challenge
        :hero_friends => [ ]
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => { },
+       :matriarch => { },
+       :hero => { },
+       :hero_friends => [ ]
+      }
+    
+  }
 
   
 end
